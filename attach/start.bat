4603bcb457cb50250a55de0b03531be1e50423d8
wuqinchao <470511113@qq.com>
2019-03-25 11:21:43 +0800

    .

---
+++ attach/start.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+
+corsair.exe --team-id H4HrChAjUDvzY72sY338Zn --cloud-asset-id wqcwindows7  --ak 8bba77b11f7a4e889ec0084ce3353ae7  --sk f7cc0044b9fe238b7baece242dd6cb224f8e486e4952461a9ff0b8aa1cff9b38
+
+endlocal
